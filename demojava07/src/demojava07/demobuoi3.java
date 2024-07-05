8adb1cf33ff22138d5ed3ceac1fcbfa319c4bcd7
BryanNguyen13 <tinnguyen100904@gmail.com>
2024-07-05 20:48:29 +0700

    first commit

---
+++ demojava07/src/demojava07/demobuoi3.java
@@ -0,0 +1,19 @@
+package demojava07;
+import java.util.*;
+public class demobuoi3{
+	// nhắc lệnh code copilot;
+	// Quy tắt đặt tên project: viết thường, không khoảng trắng, không ký tự đặt biệt
+	// đặt tên theo định dạng camelCase 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Nhập cạnh a = ");
+		double a = sc.nextInt();
+		System.out.print	("Nhập cạnh a = ");
+		double b = sc.nextInt();
+		double c = Math.sqrt(a * a + b * b);
+		System.out.println(c);
+		
+	}
+	
+}
+
